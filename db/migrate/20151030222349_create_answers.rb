28b30241a00bbca14157989567c1710899728f15
Glen Cooney <glen@glencooney.com>
2015-10-31 18:01:18 +0000

    Correct migration error - set questions as references

--- db/migrate/20151030222349_create_answers.rb
+++ db/migrate/20151030222349_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
-      t.string :question
+      t.references :question
       t.string :references
 
       t.timestamps null: false
