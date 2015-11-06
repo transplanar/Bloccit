9dc7d8f571707b41c0e7cf269a479c7142533a0d
Glen Cooney <glen@glencooney.com>
2015-11-06 16:24:08 -0500

    Fix files not properly added to master branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,12 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-  
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -24,16 +24,16 @@ class PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-    
+
     if @post.save
       flash[:notice] = "Post was updated."
       redirect_to @post
@@ -42,16 +42,16 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
-    
+
     if @post.destroy
-      flash[:notice] = "\"#{post.title}\" was deleted successfully."
-      redirect_to post_path
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to posts_path
     else
       flash[:error] = "There was an error deleting the post."
       render :show
     end
-  end 
+  end
 end
