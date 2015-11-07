aa4c8e0b03fa7c5ed6fdf151bf4ae194568f2c3f
Glen Cooney <glen@glencooney.com>
2015-11-06 23:11:54 -0500

    Checkpoint 34 Complete, Topics & Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
   get 'about' => 'welcome#about'
-  
+
   root to: 'welcome#index'
 end
