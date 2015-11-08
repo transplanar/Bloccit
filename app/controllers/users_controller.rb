d0a0ceb9464112712443b12fa8fbe8d7e59d0ad7
Glen Cooney <glen@glencooney.com>
2015-11-08 13:27:43 -0500

    Checkpoint 38 Complete, Authentication - Sign In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,21 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      create_session(@user)
       redirect_to root_path
     else
       flash[:error] = 'There was an error creating your account. Please try again.'
       render :new
     end
+  end
+
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+
   end
 end
