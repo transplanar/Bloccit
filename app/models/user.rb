4a79c6a626f6c6ee0e866a5a3e35b16fc542f0b7
Glen Cooney <glen@glencooney.com>
2015-11-08 19:33:52 -0500

    Checkpoint 40 Complete, Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts
-  
+
   before_save { self.email = email.downcase }
+  before_save {self.role ||= :member }
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -15,4 +16,6 @@ class User < ActiveRecord::Base
             format: { with: EMAIL_REGEX }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108193729) do
+ActiveRecord::Schema.define(version: 20151108215022) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20151108193729) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
