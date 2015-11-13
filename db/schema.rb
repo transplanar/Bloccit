90a11dcab1b1e2344e6addda904bbce8307151cf
Glen Cooney <glen@glencooney.com>
2015-11-13 15:06:14 -0500

    Checkpoint 43 Complete, Voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112215952) do
+ActiveRecord::Schema.define(version: 20151113191559) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20151112215952) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -88,5 +89,16 @@ ActiveRecord::Schema.define(version: 20151112215952) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Post, type: :model do
-  # let(:post){ Post.create!(title: "New Post Title", body: "New Post Body") }
-
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: 'Bloccit User', email: 'user@bloccit.com', password: 'helloworld') }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
@@ -12,6 +10,7 @@ RSpec.describe Post, type: :model do
   it { should have_many(:labels).through(:labelings) }
 
   it { should have_many(:comments) }
+  it { should have_many(:votes) }
 
   it { should belong_to(:topic) }
   it { should belong_to(:user) }
@@ -34,10 +33,49 @@ RSpec.describe Post, type: :model do
     end
   end
 
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
-  it { should validate_presence_of(:topic) }
+  describe 'voting' do
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
 
-  it { should validate_length_of(:title).is_at_least(5) }
-  it { should validate_length_of(:body).is_at_least(20) }
+    describe '#up_votes' do
+      it 'counts the number of votes with value = 1' do
+        expect(post.up_votes).to eq(@up_votes)
+      end
+    end
+
+    describe '#down_votes' do
+      it 'counts the number of votes with value = -1' do
+        expect(post.down_votes).to eq(@down_votes)
+      end
+    end
+
+    describe '#points' do
+      it 'returns the sum of all down and up votes' do
+        expect(post.points).to eq(@up_votes - @down_votes)
+      end
+    end
+
+    describe '#update_rank' do
+      it 'calculates the correct rank' do
+        post.update_rank
+        expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970, 1, 1)) / 1.day.seconds)
+      end
+
+      it 'updates the rank when an up vote is created' do
+        old_rank = post.rank
+        post.votes.create!(value: 1)
+        expect(post.rank).to eq (old_rank + 1)
+      end
+
+      it 'updates the rank when a down vote is created' do
+        old_rank = post.rank
+        post.votes.create!(value: -1)
+        expect(post.rank).to eq (old_rank - 1)
+      end
+    end
+  end
 end
