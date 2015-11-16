b055e624ec3f3b4132b83303870db0e8743db442
Glen Cooney <glen@glencooney.com>
2015-11-15 20:34:58 -0500

    Assignment 46 Complete, Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,7 +4,7 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  scope :visible_to, -> (user) { user ? [:publicly_viewable, :privately_viewable] : :publicly_viewable }
-  scope :publicly_viewable, -> {where(public: true)}
-  scope :privately_viewable, -> {where(public: false)}
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
 end
