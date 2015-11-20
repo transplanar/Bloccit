8bc71e3b5909f76420b17973252952c8bdb2ace6
Glen Cooney <glen@glencooney.com>
2015-11-20 12:15:06 -0500

    Update with some help from Chris

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
   def has_posts(user)
     user.posts.count > 0
   end
+
+  def render_posts_for(user)
+    if user.posts.any?
+      render(user.posts).html_safe
+    else
+      "<p>This user doesn't have any posts yet.</p>".html_safe
+    end
+  end
 end
